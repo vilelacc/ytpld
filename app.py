7af0e66555880a03010f76aeea876ce846264e5d
João Victor Vilela dos Santos <148297412+vilelacc@users.noreply.github.com>
2024-06-10 12:21:35 -0300

    refactoring the code
    Exception handling

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
 import os
 from pytube import YouTube, Playlist
+from pytube.exceptions import MaxRetriesExceeded
 from pytube.cli import on_progress
 from utils.functions import get_download_path, validate_folder_name
 
-full_path = get_download_path() #os.getcwd()
-playlist_url = input("Enter the playlist link: ")
-playlist = Playlist(playlist_url)
-path = os.path.join(full_path, validate_folder_name(playlist.title))
+def download_playlist(playlist_url):
+    full_path = get_download_path()  # os.getcwd()
+    playlist = Playlist(playlist_url)
+    path = os.path.join(full_path, validate_folder_name(playlist.title))
 
-if not os.path.isdir(path):
-    os.mkdir(path)
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-for url in playlist:
-    try:
-        video = YouTube(url, on_progress_callback=on_progress)
-        file_name = f"{video.title}.mp4" 
-        
-        print(f"Downloading {video.title}")
-        # check if the file already exists before downloading again
-        video_path = os.path.join(path, file_name)
+    for url in playlist:
+        try:
+            video = YouTube(url, on_progress_callback=on_progress)
+            try:
+                print(f"Downloading {video.title}")
+                stream = video.streams.get_highest_resolution()
+                stream.download(output_path=path, skip_existing=True, max_retries=2)
+            except MaxRetriesExceeded:
+                print(f"Maximum number of retries exceeded for video: {video.title}")
+        except Exception as e:
+            print(f"An error occurred while downloading the video: {e}")
+        finally:
+            print()
+            # os.system("cls" if os.name == "nt" else "clear")
 
-        if os.path.isfile(video_path):
-            print("Video already exists. Skipping...")
-            continue
-
-        stream = video.streams.get_highest_resolution()
-        stream.download(output_path=path)
-        
-        os.system("cls" if os.name == "nt" else "clear")
-
-    except Exception as e:
-        print(f"Am error occurred while downloading the video: {e}")
+if __name__ == "__main__":
+    playlist_url = input("Enter the playlist link: ")
+    download_playlist(playlist_url)
